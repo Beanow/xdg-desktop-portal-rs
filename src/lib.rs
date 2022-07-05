7336b86f9bcde5dd2f2dcdb707eb9047f8992da1
Beanow <497556+Beanow@users.noreply.github.com>
2022-07-05 12:40:31 +0200

    Reexport dbus

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,15 @@
 // SPDX-License-Identifier: MIT
 
 //! Parent window identifiers
-//! 
+//!
 //! Most portals interact with the user by showing dialogs. These dialogs should generally be placed on top of the application window that triggered them. To arrange this, the compositor needs to know about the application window. Many portal requests expect a "parent_window" string argument for this reason.
 //! Under X11, the "parent_window" argument should have the form "x11:XID", where XID is the XID of the application window in hexadecimal notation.
 //! Under Wayland, it should have the form "wayland:HANDLE", where HANDLE is a surface handle obtained with the xdg_foreign protocol.
-//! For other windowing systems, or if you don't have a suitable handle, just pass an empty string for "parent_window". 
+//! For other windowing systems, or if you don't have a suitable handle, just pass an empty string for "parent_window".
 
 mod open_uri;
 
+pub use dbus;
 pub use open_uri::*;
 
 use dbus::blocking::{BlockingSender, Proxy};
